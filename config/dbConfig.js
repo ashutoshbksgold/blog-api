4172d8e569f56cd9c5eaee7b3945dd8b42a88137
Ashutosh Maurya <ashutosh.maurya@bksgold.com>
2023-10-10 11:17:24 +0530

    changes

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const { constants } = require("../constants");
 
 exports.connectDb = () => {
-  return mongoose.connect(`${constants.DB_LOCAL}`, {
-    useNewUrlParser: true,
-  });
+  return mongoose.connect(
+    `mongodb+srv://ashutoshbksgold:ashutoshbksgold@cluster0.qejztmo.mongodb.net/blog-api`,
+    {
+      useNewUrlParser: true,
+    }
+  );
 };
